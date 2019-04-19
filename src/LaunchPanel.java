420a3afd048184f2d3641f17807b91c347a0362b
Stephen Hughes <shughes@coe.edu>
2019-04-19 09:45:40 -0500

    Initial commit

---
+++ src/LaunchPanel.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class LaunchPanel extends JPanel implements VanishListener {
+
+
+	@Override
+	public void update(VanishEvent e) {
+
+	}
+}
---
+++ src/VanishEvent.java
@@ -0,0 +1,7 @@
+import java.util.EventObject;
+
+public class VanishEvent extends EventObject {
+	public VanishEvent(Object source) {
+		super(source);
+	}
+}
